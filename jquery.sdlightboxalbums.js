459eb69380f9dadfa4b8ab64dc0ce0e9ae875ee3
seyDoggy <macagp@gmail.com>
2011-10-26 14:34:24 -0400

    "I'm not dumb. I just have a command of throughly useless information." -- Calvin, of Calvin and Hobbes

---
+++ jquery.sdlightboxalbums.js
@@ -0,0 +1,106 @@
+/*
+	# sdLightboxAlbums - prettyPhoto lightbox helper for RapidWeaver theme developers #
+
+	AUTHOR:	Adam Merrifield <http://adam.merrifield.ca>
+	VERSION: 1.0.0
+
+	UPDATES:
+	- (1.0.0) initial release
+
+	SETTINGS:
+	- css_file: is the path to the prettyPhoto css file within the RapidWeaver theme
+	- js_file: is the path to the prettyPhoto jQuery plugin
+	
+	OPTIONS:
+	- animation_speed: can be normal, slow or fast
+	- show_title: can be true or false
+	- theme: can be default, dark_rounded, dark_square, light_rounded, light_square, facebook
+
+	USAGE:
+		// basic use
+		$.sdLightboxAlbums({
+			css_file	:	'some/path/prettyPhoto.css',
+			js_file		:	'another/folder/jquery.prettyPhoto.js'
+		});
+		
+		// advanced use with options
+		$.sdLightboxAlbums({
+			css_file	:	'some/path/prettyPhoto.css',
+			js_file		:	'another/folder/jquery.prettyPhoto.js',
+			animation_speed	:	'slow',
+			show_title		:	true,
+			theme			:	'facebook'
+		});
+*/
+(function($) {
+    $.sdLightboxAlbums = function(settings) {
+        // SETTINGS
+        var opts = {
+            css_file: '',
+            js_file: '',
+			animation_speed: 'normal',
+            show_title: false,
+            theme: 'default'
+        };
+		// check for options
+        if (settings.css_file && settings.js_file) {
+			$.extend(opts, settings);
+			// VARIABLES
+	        var jq = $([]),
+	        phA = jq.add('.album-wrapper'),
+	        mA = jq.add('.movie-thumbnail-frame'),
+	        thFrame = phA.find('.thumbnail-frame');
+
+	        // ACTION
+	        if (phA.length || mA.length) {
+	            // load css (prettyPhoto)
+	            $("head").append("<link>").children(":last").attr({
+	                rel: "stylesheet",
+	                type: "text/css",
+	                href: opts.css_file
+	            });
+	            // load js (prettyPhoto)
+	            $.getScript(opts.js_file, function() {
+	                // Photo Album
+	                if (phA.length) {
+	                    // get thumbnail links and alter attributes (prettyPhoto)
+	                    thFrame.each(function() {
+	                        var thisAnch = jq.add('a', this),
+	                        thisImg = jq.add('a img', this),
+	                        thisCap = jq.add('.thumbnail-caption', this);
+	                        thisAnch.attr({
+	                            'href': thisImg.attr('src').replace(/thumb/i, 'full'),
+	                            'rel': 'prettyPhoto[gallery]',
+	                            'title': thisCap.text()
+	                        });
+	                    });
+	                } else {
+	                    // since photo album is false movie album is true
+	                    // get thumbnails links and alter attributes (prettyPhoto)
+	                    mA.each(function() {
+	                        var thisAnch = $('a', this);
+	                        var thisCap = $('.movie-thumbnail-caption', this);
+	                        var thisPage = thisAnch.attr('href');
+	                        thisAnch.removeAttr('onclick').removeAttr('onkeypress').attr({
+	                            'href': thisPage + '?iframe=true&width=75%&height=75%',
+	                            'rel': 'prettyPhoto[iframes]',
+	                            'title': thisCap.text()
+	                        });
+	                    });
+	                }
+	                // apply effects (prettyPhoto)
+	                $('a[rel^=prettyPhoto]').prettyPhoto({
+	                    animation_speed: opts.animation_speed,
+	                    show_title: opts.show_title,
+	                    theme: opts.theme
+	                });
+	            });
+	        }
+		} else {
+			// if no options detected, issue warning
+			var msg = 'The paths to the lightbox files have not been set by the theme developer!';
+			alert(msg);
+			console.log(msg);
+		}
+    }
+})(jQuery);
