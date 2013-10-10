dc4256fafc467172b3e66b047e8fbf7eb536783b
Adam Merrifield <macagp@gmail.com>
2013-10-10 07:06:36 -0400

    Set ajax url as variable and removed second ajax attempt.

--- jquery.sdlightboxalbums.js
+++ jquery.sdlightboxalbums.js
@@ -5,33 +5,33 @@
  * VERSION: 1.2.3
  *
  * SETTINGS:
- * - css_file: is the path to the prettyPhoto css file within the RapidWeaver theme
- * - js_file: is the path to the prettyPhoto jQuery plugin
+ *  - css_file: is the path to the prettyPhoto css file within the RapidWeaver theme
+ *  - js_file: is the path to the prettyPhoto jQuery plugin
  *
- * OPTIONS:
- * - animation_speed: can be normal, slow or fast
- * - show_title: can be true or false
- * - theme: can be default, dark_rounded, dark_square, light_rounded, light_square, facebook
+ *  OPTIONS:
+ *  - animation_speed: can be normal, slow or fast
+ *  - show_title: can be true or false
+ *  - theme: can be default, dark_rounded, dark_square, light_rounded, light_square, facebook
  *
- * USAGE:
- * // basic use
- * jQuery.sdLightboxAlbums({
- *      css_file :   'some/path/prettyPhoto.css',
- *      js_file  :   'another/folder/jquery.prettyPhoto.js'
- * });
+ *  USAGE:
+ *      basic use
+ *          $.sdLightboxAlbums({
+ *              css_file   :   "value", 'some/path/prettyPhoto.css',
+ *              js_file    :   'another/folder/jquery.prettyPhoto.js'
+ *          });
  *
- * // advanced use with options
- * jQuery.sdLightboxAlbums({
- *      css_file    :   'some/path/prettyPhoto.css',
- *      css_local   :   RwGet.pathto('css/prettyPhoto.css'),
- *      js_file     :   'another/folder/jquery.prettyPhoto.js'
- *      js_local    :   RwGet.pathto('scripts/jquery.prettyPhoto.js'),
- *      animation_speed :   'slow',
- *      show_title      :   true,
- *      theme           :   'facebook',
- *      social_tools    :   false
- * });
- */
+ *      advanced use with options
+ *          $.sdLightboxAlbums({
+ *              css_file    :   'some/path/prettyPhoto.css',
+ *              css_local   :   RwGet.pathto('css/prettyPhoto.css'),
+ *              js_file     :   'another/folder/jquery.prettyPhoto.js'
+ *              js_local    :   RwGet.pathto('scripts/jquery.prettyPhoto.js'),
+ *              animation_speed :   'slow',
+ *              show_title      :   true,
+ *              theme           :   'facebook',
+ *              social_tools    :   false
+ *          });
+*/
 (function($) {
     "use strict";
     jQuery.sdLightboxAlbums = function(settings) {
@@ -61,6 +61,13 @@
         } else {
             this_js = settings.js_file;
         }
+
+        var throwError = function () {
+            // if no options detected, issue warning
+            var msg = 'The paths to the lightbox files have not been set by the theme developer!';
+            alert(msg);
+            console.log(msg);
+        };
 
         // check for albums
         if (phA.length || mA.length) {
@@ -116,6 +123,7 @@
                     });
                 };
                 // load js (prettyPhoto)
+                // remote attempt
                 jQuery.ajax({
                     url: this_js,
                     dataType: "script",
@@ -125,17 +133,23 @@
                         doPrettyPhoto(this_css);
                     },
                     fail: function () {
-                        // if no options detected, issue warning
-                        var msg = 'The paths to the lightbox files have not been set by the theme developer!';
-                        alert(msg);
-                        console.log(msg);
+                        throwError()
                     }
                 });
+                // local attempt
+                if ((typeof jQuery.fn.prettyPhoto != 'function') && (opts.css_local && opts.js_local)) {
+                    jQuery.ajax({
+                        url: opts.js_local,
+                        dataType: "script",
+                        async: false,
+                        crossDomain: false,
+                        success: function(){
+                            doPrettyPhoto(opts.css_local);
+                        }
+                    });
+                }
             } else {
-                // if no options detected, issue warning
-                var msg = 'The paths to the lightbox files have not been set by the theme developer!';
-                alert(msg);
-                console.log(msg);
+                throwError();
             }
         }
     };
